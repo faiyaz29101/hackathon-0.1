7e59c28e4924d15b880e74c538775c31665ab9ad
faiyaz <legendfaiyaz4166@gmail.com>
2023-06-08 03:11:37 +0600

    final

--- index.js
+++ index.js
@@ -2,12 +2,9 @@ let http = require('http');
 let fs = require('fs');
 
 let server = http.createServer((req, res) => {
-  // Set content type for HTML responses
   res.setHeader('Content-Type', 'text/html');
 
-  // Handle requests based on the route
   if (req.url === '/' && req.method === 'GET') {
-    // Serve file1.html on GET /
     fs.readFile('index.html', (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -18,7 +15,6 @@ let server = http.createServer((req, res) => {
       }
     });
   } else if (req.url === '/route1' && req.method === 'GET') {
-    // Serve file2.html on GET /route1
     fs.readFile('projects.html', (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -29,7 +25,6 @@ let server = http.createServer((req, res) => {
       }
     });
   } else if (req.url === '/route2' && req.method === 'GET') {
-    // Serve file3.html on GET /route2
     fs.readFile('about.html', (err, data) => {
       if (err) {
         res.statusCode = 500;
@@ -40,13 +35,10 @@ let server = http.createServer((req, res) => {
       }
     });
   } else {
-    // Return 404 Not Found for any other route
     res.statusCode = 404;
     res.end('Webpage not found');
   }
 });
-
-// Start the server on port 3000
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+server.listen(1010, () => {
+  console.log('Server is running on http://localhost:1010');
 });
